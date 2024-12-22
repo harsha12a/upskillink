b592d2ac71c1f43015dec2fa47bd99064662e24c
harsha12a <harsha.pss2005@gmail.com>
2024-12-22 15:36:43 +0530

    add:2nd page

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Second() {
   return (
     <section className="w-full h-screen relative z-[3]">
-        <div className="w-full h-screen bg-black"></div>
+        <div className="w-full h-screen bg-black">
+          <div className='text-white text-3xl text-center pt-20 font-bold'>Mentors From Your Dream Companies</div>
+          <div className='text-[gray] pt-3 text-sm text-center font-thin'>Connect, Learn and Grow with the Help of Top mentors</div>
+        </div>
       </section>
   )
 }
