88a036a0f8f26a62a3f249bf722d30320351a459
SeemaJabeen <22501a05g6@pvpsit.ac.in>
2024-12-26 20:49:55 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ import angleup from "../assets/angleup.png";
 
 function Footer() {
   return (
-    <section className="w-full min-h-[800px] md:min-h-[800px] lg:min-h-[800px] absolute bg-black mt-[200px]">
-      <div className="px-5 md:px-[100px] flex flex-col md:flex-row md:gap-x-[200px] mb-[25px] text-[18px]">
+    <section className="w-full min-h-[500px] md:min-h-[600px] lg:min-h-[650px] absolute bg-black mt-[100px]">
+      <div className="md:px-[100px] flex flex-col md:flex-row md:gap-x-[100px] mb-[25px] text-[18px]">
         
         <ul className="mb-8 md:mb-0">
           <h1 className="text-white mb-4 font-bold text-lg md:text-xl">For Candidates</h1>
@@ -41,24 +41,30 @@ function Footer() {
           </div>
         </ul>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center md:items-start">
           <h1 className="text-white font-bold text-lg md:text-xl mb-4">Social Media</h1>
-          <div className="flex gap-4">
+          <div className="flex flex-wrap gap-4 justify-center md:justify-start md:gap-6">
             {[
               { img: facebook, alt: "Facebook" },
               { img: twitter, alt: "Twitter" },
               { img: linkedin, alt: "LinkedIn" },
               { img: instagram, alt: "Instagram" },
             ].map((icon, index) => (
-              <div
-                key={index}
-                className="h-[50px] w-[50px] bg-gray-800 rounded-full flex justify-center items-center"
-              >
-                <img src={icon.img} alt={icon.alt} className="h-2/4 w-2/4" />
-              </div>
+            <div
+              key={index}
+              className="h-[50px] w-[50px] bg-gray-800 rounded-full flex justify-center items-center"
+            >
+              <img
+              src={icon.img}
+              alt={icon.alt}
+              className="h-auto max-h-[25px] max-w-[25px] object-contain"
+              />
+            </div>
             ))}
           </div>
+      
         </div>
+    
       </div>
 
    
@@ -66,19 +72,17 @@ function Footer() {
         <img src={angleup} alt="Scroll Up" className="h-2/4 w-2/4" />
       </div>
 
-      <h1 className="text-white font-bold text-[40px] sm:text-[60px] md:text-[100px] lg:text-[200px] xl:text-[350px] text-center py-2 leading-none">Upskillink</h1>
-
-
+      <h1 className="text-white font-bold text-5xl md:text-8xl lg:text-[210px] text-center py-2 ">Upskillink</h1>
     
-      <div className="max-w-full mt-12 h-[150px] md:h-[120px] bg-gray-800 rounded-[20px] md:rounded-[40px] flex flex-col md:flex-row justify-between items-center px-7 md:px-10 md:mx-auto md:max-w-[1700px] lg:mt-9">
+      <div className="max-w-full mt-12  sm:h-[80px] md:h-[100px] bg-gray-800 rounded-[20px] md:rounded-[40px] flex flex-col md:flex-row justify-between items-center px-7 md:px-10 md:mx-auto md:max-w-[1700px] lg:mt-9">
         <div className="mb-3 md:mb-0">
-          <p className="text-left text-white text-lg md:text-xl">
+          <p className="text-left text-white text-lg md:text-2xl">
             Copyright©2024Upskillink
           </p>
         </div>
 
-        <div className="flex text-white md:text-xl">
-          <ul className="flex flex-wrap justify-center space-x-4 md:space-x-6">
+        <div className="flex text-white md:text-2xl">
+          <ul className="flex flex-wrap justify-center space-x-20  ">
             <li className="hover:text-gray-400 cursor-pointer">Term of Use</li>
             <li className="hover:text-gray-400 cursor-pointer">Privacy Policy</li>
             <li className="hover:text-gray-400 cursor-pointer">Licensing</li>
